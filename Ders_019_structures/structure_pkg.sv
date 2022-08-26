8b89bd3d6d20f305cca2cfd0d9417e7ada901f78
Muhammed KOCAOĞLU <kocaoglu_muhammed@outlook.com>
2022-08-26 19:58:59 +0300

    structure file updated

--- Ders_019_structures/structure_pkg.sv
+++ Ders_019_structures/structure_pkg.sv
@@ -23,20 +23,20 @@
 package structure_pkg;
 
 typedef struct {
-    integer DATA_ID;
-    logic  [7:0]  DATA_HEADER;
-    logic  [7:0]  DATA_FOOTER;
-    integer DATA_START_ADDR;
-    integer DATA_END_ADDR;
-    integer DATA_LENGTH;
+    integer         DATA_ID;
+    logic  [7:0]    DATA_HEADER;
+    logic  [7:0]    DATA_FOOTER;
+    integer         DATA_START_ADDR;
+    integer         DATA_END_ADDR;
+    integer         DATA_LENGTH;
 } my_structure;
 
 localparam my_structure s0 = '{
-    DATA_ID : 0,
-    DATA_HEADER : 8'hAB,
-    DATA_FOOTER : 8'hFC,
+    DATA_ID         : 0,
+    DATA_HEADER     : 8'hAB,
+    DATA_FOOTER     : 8'hFC,
     DATA_START_ADDR : 7,
-    DATA_END_ADDR : 42,
-    DATA_LENGTH : 42 - 7 + 1
+    DATA_END_ADDR   : 42,
+    DATA_LENGTH     : 42 - 7 + 1
 };
 endpackage
